aba7e4733d2dc854a41aeb6e9f78cb2a1d3bb0f6
HWON0720 <hwon0720@gmail.com>
2022-05-01 21:31:30 +0900

    2-2

---
+++ javajungsuk_basic/ch02_Variable/2-2.java
@@ -0,0 +1,11 @@
+class Ex2_2 {
+  public static void main(String args[]) {
+    System.out.println("Hello, world"); // 화면에 Hello, world가 출력된다.
+    System.out.println("3+5=");         // 화면에 3+5=를 출력하고 줄바꿈 안 한다.
+    System.out.println(3+5);            // 화면에 8이 출력된다.
+  }
+}
+
+// 결과
+// Hello, world
+// 3+5=8
