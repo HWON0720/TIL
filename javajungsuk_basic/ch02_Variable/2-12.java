3d732344538d1ca39a0eafef76f46f48e59eb0e7
HWON0720 <hwon0720@gmail.com>
2022-05-02 16:18:33 +0900

    2-12 : 타입 간의 변환방법

---
+++ javajungsuk_basic/ch02_Variable/2-12.java
@@ -0,0 +1,18 @@
+public class Ex2_12 {
+    public static void main(String[] args) {
+        String str = "3";
+
+        System.out.println(str.charAt(0) - '0');
+        System.out.println('3' - '0' + 1);
+        System.out.println(Integer.parseInt("3") + 1);
+        System.out.println("3" + 1);
+        System.out.println((char) (3 + '0'));
+    }
+}
+
+// 결과
+// 3
+// 4
+// 4
+// 31
+// 3
