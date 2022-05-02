75be2c78ec329d2e39cbcb5c28b6746a5c9027ed
HWON0720 <hwon0720@gmail.com>
2022-05-02 11:11:24 +0900

    2-4 : 변수의 선언과 저장

---
+++ javajungsuk_basic/ch02_Variable/2-4.java
@@ -0,0 +1,13 @@
+class Ex2_4 {
+  public static void main(String args[]) {
+    int x = 5;              // inx t;와 x = 5;를 한 줄로 합칠 수 있다.
+    System.out.println(x);  // 화면에 x의 값인 5 출력.
+    
+    x = 10;                 // 변수 x에 10을 저장, 기존에 저장되어 있던 5는 지워진다.
+    System.out.println(x);  // 화면에 x의 값인 10 출력.
+  }
+}
+
+// 결과
+// 5
+// 10
