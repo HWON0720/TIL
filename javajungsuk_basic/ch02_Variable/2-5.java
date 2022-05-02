7ef7aa0531cc122637ffe2960e6121c1483198dc
HWON0720 <hwon0720@gmail.com>
2022-05-02 11:20:26 +0900

    2-5 : 변수의 선언과 저장

---
+++ javajungsuk_basic/ch02_Variable/2-5.java
@@ -0,0 +1,16 @@
+class Ex2_5 {
+  public static void main(String args[]) {
+    int x = 10;
+    int y = 5;
+    System.out.println(x+y);
+    System.out.println(x-y);
+    System.out.println(x*y);
+    System.out.println(x/y);
+  }
+}
+
+// 결과
+// 15
+// 5
+// 50
+// 2
