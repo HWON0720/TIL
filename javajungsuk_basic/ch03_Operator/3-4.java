4c82c988d85b67f2b5a7579ed20dc1d0c8f158cd
HWON0720 <hwon0720@gmail.com>
2022-05-03 17:20:09 +0900

    3-4 : 부호 연산자

---
+++ javajungsuk_basic/ch03_Operator/3-4.java
@@ -0,0 +1,15 @@
+public class Ex3_4 {
+    public static void main(String[] args) {
+        int i = -10;
+        i = +i;
+        System.out.println(i);
+
+        i = -10;
+        i = -i;
+        System.out.println(i);
+    }
+}
+
+// 결과
+// -10
+// 10
