86d1322f74551c5208b3d97c5aff836a1c0f5f14
HWON0720 <hwon0720@gmail.com>
2022-05-03 17:23:00 +0900

    3-5 : 형변환 연산자

---
+++ javajungsuk_basic/ch03_Operator/3-5.java
@@ -0,0 +1,12 @@
+public class Ex3_5 {
+    public static void main(String[] args) {
+        double d = 85.4;
+        int score = (int) d;
+        System.out.println("score = " + score);
+        System.out.println("d = " + d);
+    }
+}
+
+// 결과
+// score = 85
+// d = 85.4 (형변환 후에도 피연산자에는 아무런 변화가 없다.
