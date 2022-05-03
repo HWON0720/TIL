b186fdc8c08881f762e2c4205e8d2b34f6b07149
HWON0720 <hwon0720@gmail.com>
2022-05-03 17:30:15 +0900

    3-7 : 산술 변환

---
+++ javajungsuk_basic/ch03_Operator/3-7.java
@@ -0,0 +1,10 @@
+public class Ex3_7 {
+    public static void main(String[] args) {
+        System.out.println(5 / 2);
+        System.out.println(5 / (float) 2);  // (float) 5 / 2의 결과도 동일
+    }
+}
+
+// 결과
+// 2
+// 2.5
