16ff0ab622a657263e0daf6edee97d6a8f273c1b
HWON0720 <hwon0720@gmail.com>
2022-05-06 18:00:26 +0900

    4-12 : while문 예제1

---
+++ javajungsuk_basic/ch04_if, switch, for, while statement/4-12.java
@@ -0,0 +1,16 @@
+class Ex4_12 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i-- != 0) {
+            System.out.println(i + " - I can do it.");
+        }
+    }
+}
+
+// 결과
+// 4 - I can do it.
+// 3 - I can do it.
+// 2 - I can do it.
+// 1 - I can do it.
+// 0 - I can do it.
