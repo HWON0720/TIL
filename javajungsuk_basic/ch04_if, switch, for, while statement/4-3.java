76159eb658901548dde4be9c13a5286ed3936985
HWON0720 <hwon0720@gmail.com>
2022-05-06 16:58:59 +0900

    4-3 : if-else문

---
+++ javajungsuk_basic/ch04_if, switch, for, while statement/4-3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;   // Scanner 클래스를 사용하기 위해 추가
+
+class Ex4_3 {
+    public static void main(String[] args) {
+        System.out.println("숫자를 하나 입력하세요.>");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();  // 화면을 통해 입력받은 숫자를 input에 저장
+
+        if (input == 0) {
+            System.out.println("입력하신 숫자는 0입니다.");
+        } else {    // input != 0인 경우
+            System.out.println("입력하신 숫자는 0이 아닙니다.");
+        } 
+    }   // main의 끝
+}
+
+// 결과 1
+// 숫자를 하나 입력하세요.>5
+// 입력하신 숫자는 0이 아닙니다.
+
+// 결과 2
+// 숫자를 하나 입력하세요.>0
+// 입력하신 숫자는 0입니다.
