0d481c25fe470f3ae5fc2eb38c3dabba59004661
HWON0720 <hwon0720@gmail.com>
2022-05-13 17:54:35 +0900

    add java_basic/java_basic

---
+++ java_basic/java_basic/src/StringExample.java
@@ -0,0 +1,10 @@
+public class StringExample {
+    public static void main(String[] args) {
+        System.out.println("나는 문자열");   // 여러 개의 문자(=문자열)를 담을 수 있음
+        System.out.println('나');    // 문자 하나만 담을 수 있음
+
+        System.out.println("안녕, 나는 \"자바\"야.");  // 쌍따옴표(") 안에 쌍따옴표(") 사용 시, \를 앞에 붙여 사용
+
+        System.out.println("치킨은 살 안 쪄요.\n살은 내가 쪄요.");  // 표준출력 내부에서 줄바꿈 시, \n 사용
+    }
+}
